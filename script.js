0a38e2dfc600b3251f1e8d6a1868dc965fce4e05
Mae Cagadas <32762531+rogueassassin11@users.noreply.github.com>
2020-11-27 21:32:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2. Check if score is 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //Finish the game
       playing = false;
       diceEl.classList.add('hidden');
